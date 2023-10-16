b19d67b2550255e7b64420e447d8155b98d1a437
Debora Amaral <59532015+DeboraAmaral@users.noreply.github.com>
2023-10-16 00:42:47 -0300

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,27 @@
+import {} from 'react';
+import icone01 from '../assets/img/instagram.png'
+import icone02 from '../assets/img/linkedin.png'
+import icone03 from '../assets/img/github.png'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-logo">
+            <h1>Veggie Bliss</h1>
+            <p>© 2023 - Todos os direitos reservados</p>
+          </div>
+          <div className="icones">
+            <a href="https://www.instagram.com/levyv_/"><img src={icone01}></img></a>
+            <a href="https://www.linkedin.com/in/l%C3%ADvianamba/"><img src={icone02}></img></a>
+            <a href="https://github.com/DeboraAmaral/projetoweb"><img src={icone03}></img></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
